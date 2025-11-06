6789c2570fa8deb217c21d684ccdced3cc0a276f
Krish Bhalala <bhalalks@myumanitoba.ca>
2025-11-06 08:39:01 -0600

    Removed the discord link and changed it to active minds podcast

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -28,7 +28,7 @@ const footer=`
                         <h4>Follow Us</h4>
                         <div class="social-icons">
                             <a href="https://www.instagram.com/uofm_activeminds" target="_blank"><i id="insta"> <img src="assets/instagram.png" alt=""></i></a>
-                            <a href="https://discord.gg/uETydshJAV" target="_blank"><i id="discord"> <img src="assets/discord.png" alt=""></i></a>
+                            <a href="https://umfm.com/programming/shows/the-active-mindset" target="_blank"><i id="podcast"> <img src="assets/podcast_color.png" alt=""></i></a>
                             <a href="https://t.me/+xaofJQ6xy4UxZjg5" target="_blank"><i id="telegram"> <img src="assets/telegram.png" alt=""></i></a>
                             <a href="https://linktr.ee/activemindsumanitoba" target="_blank"><i id="linktree"> <img src="assets/linktree.png" alt=""></i></a>
                         </div>
